ed2f609eca7b1619191748c0ae0f36d8778bb448
kvadityasrivatsa <k.v.aditya@research.iiit.ac.in>
2024-10-18 19:33:04 +0400

    updated launch data path

--- emnlp2021/src/launch_test.sh
+++ emnlp2021/src/launch_test.sh
@@ -1,6 +1,6 @@
 python pb_sentence_entropy.py \
-    --data_path /Users/kvaditya/threads/mbzuai/uid-dialogue/emnlp2021/data/true/pb_gpt2-ft.csv \
-    --out_path /Users/kvaditya/threads/mbzuai/uid-dialogue/emnlp2021/src/launch_test_output \
+    --data_path ../data/true/pb_gpt2-ft.csv \
+    --out_path ./launch_test_output \
     --model_name "gpt2" \
     --right_context 4 \
     --max_seq_len 64 \

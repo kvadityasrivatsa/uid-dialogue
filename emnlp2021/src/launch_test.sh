cdd8a821b8208622375de69d62bfc65791930b96
KV Aditya Srivatsa <k.v.aditya@research.iiit.ac.in>
2024-10-19 15:51:53 +0400

    rem ft tangled

--- emnlp2021/src/launch_test.sh
+++ emnlp2021/src/launch_test.sh
@@ -1,12 +1,9 @@
-python ft_pb_sentence_entropy.py \
+python pb_sentence_entropy.py \
     --data_path ../data/true/pb_gpt2-ft.csv \
     --out_path ./launch_test_output \
     --model_name "gpt2" \
-    --model_path ./ft_gpt2/ \
     --right_context 4 \
     --max_seq_len 64 \
     --add_speaker_ids \
-    --per_gpu_batch_size 64 \
-    --learning_rate 5e-5 \
-    --num_epochs 30 \
-    --seed 42
+    --per_gpu_batch_size 1 \
+    --seed 7777 

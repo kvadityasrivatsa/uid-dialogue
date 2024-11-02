1c9cd24faba85b11e7f04d0584251fe93bc3181c
alokdebnath <debnatha@tcd.ie>
2024-11-02 14:43:20 +0000

    added scripts, output after training, and the entropy .csv file

--- emnlp2021/src/launch_test.sh
+++ emnlp2021/src/launch_test.sh
@@ -1,10 +1,10 @@
-python pbft_scores.py \
+python3 pbft_scores.py \
     --data_path ../data/true/pb_gpt2-ft.csv \
     --out_path ./launch_test_output \
     --model_name "roberta" \
-    --model_path "<path>" \
+    --model_path ./roberta-pbft/checkpoint-800 \
     --right_context 4 \
     --max_seq_len 64 \
     --add_speaker_ids \
     --per_gpu_batch_size 1 \
-    --seed 7777 
+    --seed 7777
---
+++ emnlp2021/src/roberta-finteune.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH -p long    # which partition to run on ('compute' is default)
+#SBATCH -J entropy-empathy    # arbitrary name for the job (you choose)
+#SBATCH --output=roberta-pbft-entropy-%j.out
+#SBATCH --error=roberta-pbft-entropy-%j.err
+#SBATCH --cpus-per-task=4    # tell Slurm how many CPU cores you need, if different from default; your job won't be able to use more than this
+#SBATCH --mem=50000    # how much RAM you need (30GB in this case), if different from default; your job won't be able to use more than this
+#SBATCH -t 1-02:30:00    # maximum execution time: in this case one day, two hours and thirty minutes (optional)
+
+
+
+# Uncomment the following to get a log of memory usage; NOTE don't use this if you plan to run multiple processes in your job and you are placing "wait" at the end of the job file, else Slurm won't be able to tell when your job is completed!
+
+vmstat -S M {interval_secs} >> memory_usage_$SLURM_JOBID.log &
+
+module load anaconda3-2024.02-1
+source /home/adebnath/anaconda3/bin/activate
+eval "${conda shell.bash hook}"
+
+# Your commands here
+conda activate base
+python3 roberta_ft.py
+conda deactivate

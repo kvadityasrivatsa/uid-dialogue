149d82cac295110b816cf1c9d93d35a8db953e31
KV Aditya Srivatsa <k.v.aditya@research.iiit.ac.in>
2024-11-28 08:51:46 +0400

    repaired empdial ft

--- emnlp2021/src/launch_test.sh
+++ emnlp2021/src/launch_test.sh
@@ -1,8 +1,8 @@
 python3 pbft_scores.py \
     --data_path ../../conversation_corpora/empathetic_dialogues_sent_tokenized.csv \
-    --out_path ./launch_test_output/empdial/base \
+    --out_path ./launch_test_output/empdial \
     --model_name roberta \
-    --model_path roberta-base \
+    --model_path ../roberta-empathetic_dialogues_sent_tokenized-finetuned_final \
     --right_context 4 \
     --max_seq_len 64 \
     --add_speaker_ids \
